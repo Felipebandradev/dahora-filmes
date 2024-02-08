44da2b1dbe9dea7872c6ecf315b68313739e69b2
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-02-08 09:36:41 -0300

    feat: COMPONENTES

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <Text>App</Text>
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.viewLogo}>
+          <Text>Dá Hora Filmes</Text>
+        </View>
+        <View style={estilos.viewBotoes}>
+          <Button title="Buscar Filmes" />
+          <Button title="Favoritos" />
+        </View>
+        <View style={estilos.viewRodape}>
+          <Button title="privacidade" />
+          <Button title="Sobre" />
+        </View>
       </SafeAreaView>
     </>
   );
