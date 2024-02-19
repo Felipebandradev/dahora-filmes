86e503b873a484513dee69bac74eea2f29154f8e
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-02-19 10:29:53 -0300

    feat: ajustes na home

--- App.js
+++ App.js
@@ -1,71 +1,11 @@
-import {
-  Image,
-  Pressable,
-  SafeAreaView,
-  StatusBar,
-  Text,
-  View,
-} from "react-native";
-import { MaterialCommunityIcons, Fontisto, Ionicons } from "@expo/vector-icons";
-import logo from "./assets/images/logo.png";
-
-import { estilosInicio } from "./src/stylesheet/estilos";
 import SafeContainer from "./src/components/SafeContainer";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
     <>
       <SafeContainer>
-        <View style={estilosInicio.viewLogo}>
-          <Image source={logo} style={estilosInicio.logo} />
-          <Text style={estilosInicio.titulo}>Dá Hora Filmes</Text>
-        </View>
-        <View style={estilosInicio.viewBotoes}>
-          <Pressable style={estilosInicio.botao}>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-                gap: 2,
-              }}
-            >
-              <MaterialCommunityIcons
-                name="filmstrip-box-multiple"
-                size={16}
-                color="white"
-              />
-              <Text style={estilosInicio.textoBotao}>Buscar Filmes</Text>
-            </View>
-          </Pressable>
-          <Pressable style={estilosInicio.botao}>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-                gap: 2,
-              }}
-            >
-              <Fontisto name="favorite" size={16} color="white" />
-              <Text style={estilosInicio.textoBotao}>Favoritos</Text>
-            </View>
-          </Pressable>
-        </View>
-        <View style={estilosInicio.viewRodape}>
-          <Pressable style={estilosInicio.botaoRodape}>
-            <Text style={estilosInicio.textoBotao}>
-              <Ionicons name="lock-closed" size={24} color="white" />
-              Privacidade
-            </Text>
-          </Pressable>
-          <Pressable style={estilosInicio.botaoRodape}>
-            <Text style={estilosInicio.textoBotao}>
-              <Ionicons name="information-circle" size={24} color="white" />
-              Sobre
-            </Text>
-          </Pressable>
-        </View>
+        <Home />
       </SafeContainer>
     </>
   );
---
+++ src/screens/Home.js
@@ -0,0 +1,62 @@
+import { Image, Pressable, Text, View } from "react-native";
+import SafeContainer from "../components/SafeContainer";
+import { estilosInicio } from "../stylesheet/estilos";
+import { MaterialCommunityIcons, Fontisto, Ionicons } from "@expo/vector-icons";
+import logo from "../../assets/images/logo.png";
+
+export default function Home() {
+  return (
+    <SafeContainer>
+      <View style={estilosInicio.viewLogo}>
+        <Image source={logo} style={estilosInicio.logo} />
+        <Text style={estilosInicio.titulo}>Dá Hora Filmes</Text>
+      </View>
+      <View style={estilosInicio.viewBotoes}>
+        <Pressable style={estilosInicio.botao}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <MaterialCommunityIcons
+              name="filmstrip-box-multiple"
+              size={16}
+              color="white"
+            />
+            <Text style={estilosInicio.textoBotao}>Filmes</Text>
+          </View>
+        </Pressable>
+        <Pressable style={estilosInicio.botao}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <Fontisto name="favorite" size={16} color="white" />
+            <Text style={estilosInicio.textoBotao}>Favoritos</Text>
+          </View>
+        </Pressable>
+      </View>
+      <View style={estilosInicio.viewRodape}>
+        <Pressable style={estilosInicio.botaoRodape}>
+          <Text style={estilosInicio.textoBotao}>
+            <Ionicons name="lock-closed" size={24} color="white" />
+            Privacidade
+          </Text>
+        </Pressable>
+        <Pressable style={estilosInicio.botaoRodape}>
+          <Text style={estilosInicio.textoBotao}>
+            <Ionicons name="information-circle" size={24} color="white" />
+            Sobre
+          </Text>
+        </Pressable>
+      </View>
+    </SafeContainer>
+  );
+}
