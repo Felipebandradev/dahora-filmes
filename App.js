ba39d35f47630f78bfe920693671207919121259
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-02-26 09:18:23 -0300

    feat: configuração de screen home

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import SafeContainer from "./src/components/SafeContainer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Home from "./src/screens/Home";
 import Privacidade from "./src/screens/Privacidade";
 import Sobre from "./src/screens/Sobre";
 
+// Criação e inicialização do mecanismo Stack
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <>
-      <SafeContainer>
-        {/* <Sobre/> */}
-        {/* <Home /> */}
-        <Privacidade />
-      </SafeContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
