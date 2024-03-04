a41cca7c9e56b0aa425dc45d40edac87db34a04e
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-03-04 11:59:24 -0300

    feat: uso do use efect e useState

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,15 +1,39 @@
 import { Text, View } from "react-native";
 import SafeContainer from "../components/SafeContainer";
 import { estiloSobre, estilosResultado } from "../stylesheet/estilos";
+import { api, apiKey } from "../services/api-moviedb";
+import { useEffect, useState } from "react";
 
 /* Prop Route
 Prop Especial e definida pelo React Navigation.
 Através dela que é possível acessar valores passados
 por meio de navegação entre telas. */
 export default function Resultados({ route }) {
-  // Capturando o parâmetro filme vindo de BuscarFilmes
+  /* State para gerenciar os resultados da busca na API */
+  const [resultados, setResultados] = useState([]);
 
+  /*  Capturando o parâmetro filme vindo de BuscarFilmes */
   const { filme } = route.params;
+
+  useEffect(() => {
+    async function buscarFilmes() {
+      try {
+        const resposta = await api.get("/search/movie", {
+          params: {
+            include_adult: false,
+            language: "pt-BR",
+            query: filme,
+            api_key: apiKey,
+          },
+        });
+        console.log(resposta.data);
+      } catch (error) {
+        console.error("Deu Ruim: " + error.message);
+      }
+    }
+    buscarFilmes();
+  }, []);
+
   return (
     <SafeContainer>
       <View style={estiloSobre.subContainer}>
